7afa01295dcfd84f6ed5bd314e0d0aa82e9b5bad
BartyDash <writetobartek@gmail.com>
2024-05-20 14:47:34 +0200

    better email sending: form & script

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
-document.getElementById('newsletterForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const email = document.getElementById('emailaddress').value;
+const button = document.getElementById("button").addEventListener('click', submit);
 
-    fetch('https://script.google.com/macros/s/AKfycbxk7Nwnpjd0IK47FerhrMmlpGQng5bks-FYEohUXRySn-dQMDSXqYOoqULQZQCKhvpH9Q/exec', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email: email })
-    })
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('responseMessage').innerText = 'Dziękujemy za zapisanie się!';
-    })
-    .catch(error => {
-        document.getElementById('responseMessage').innerText = 'Wystąpił błąd: ' + error.message;
-    });
-});
+async function submit() {
+    // Hubspot's base API url
+    let base_url = "https://api.hsforms.com/submissions/v3/integration/submit";
+    
+    // Our portalId
+    let portal_id = "144715652";
+    
+    // Our formId
+    let form_id = "af2a733d-8790-4d20-b8fa-49571aa6c6ff";
+    
+    // Construct the request url
+    let request_url = base_url + "/" + portal_id + "/" + form_id;
+    
+    // Selecting the email input element and get its value
+    var email = document.getElementById("emailaddress").value;
+    let body = {
+              "submittedAt": (new Date()).getTime(),
+              "fields": [
+                  {
+                      "objectTypeId": "0-1",
+                      "name": "email",
+                      "value": email
+                  }
+              ]
+          }
+          await fetch(request_url, {
+              method: 'POST',
+              mode: 'cors',
+              headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(body)
+          }).then(alert("Success!"))
+  }
